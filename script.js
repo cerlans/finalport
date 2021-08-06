644fccb799f06d4221fffd7a6aa29a28416e3685
Kevin <35633054+cerlans@users.noreply.github.com>
2021-08-06 12:26:04 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const swup = new Swup();
+
+
+
+let controllerNav = document.getElementById('controllerNav');
+let hiddenMenu = document.getElementById('hiddenMenu');
+let closeMark = document.getElementById('closeMark')
+
+if(window.innerWidth < 768){
+      
+    console.log(controllerNav)
+    /* Need to add a class instead of directly injecting styles */
+    controllerNav.addEventListener('click',()=>{
+      hiddenMenu.classList.remove('toggleSmall')
+      hiddenMenu.classList.add('toggle')
+    })
+
+    /* When Something is clicked in the menu, close the menu as-well*/
+
+    hiddenMenu.addEventListener('click',(event)=>{
+    if(event.target.classList.contains('click') ) {
+      console.log(event.target)
+      hiddenMenu.classList.remove('toggle')
+      hiddenMenu.classList.add('toggleSmall')
+    }
+    else {
+      console.log('random')
+    }
+
+    })
+
+  document.addEventListener('mouseup',function(event){
+    if(event.target != hiddenMenu){
+      hiddenMenu.classList.remove('toggle')
+      hiddenMenu.classList.add('toggleSmall')
+    }
+  })
+}
